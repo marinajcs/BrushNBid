343de0366910487d0c482c158f3ba7404025dfa8
Marina Jun Carranza Sánchez <marinacarranza163@gmail.com>
2024-10-27 14:44:00 +0100

    feat: creación de la interfaz starts #16

---
+++ src/app.ts
@@ -0,0 +1,1 @@
+const appDiv = document.getElementById('app')!;
---
+++ webpack.client.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './static/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+    watchFiles: ['src/**/*'],
+    proxy: [
+      {
+        context: ['/api'], // Ruta que debe redirigirse al backend
+        target: 'http://localhost:3000', // URL del servidor backend
+        changeOrigin: true,
+      },
+    ],
+  },
+};
